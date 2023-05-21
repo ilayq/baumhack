5bb007fea8b5a41937c02441d08d061a30d1b6a3
misha <ilayfeeed@gmail.com>
2023-05-21 15:09:06 +0300

    sort

--- back/get_table_handler.py
+++ back/get_table_handler.py
@@ -5,55 +5,6 @@ import pydantic
 from Filter import FilterList
 from get_csv import get_rows_from_csv
 
-# todo filters
-# async def get_table_handler(page: int, count: int,
-#                             sort: int = -1, reverse: bool = False, search: str = None,
-#                             filters: FilterList = None) -> List[List[str]]:
-#     iterator = get_rows_from_csv()
-#     response = []
-#     if filters:
-#         temp_response = list(get_rows_from_csv())
-#         for filt in filters.filters:
-#             if filt.filter_ == 'l':
-#                 temp_response = list(filter(lambda element: element[filt.column] < filt.value, temp_response))
-#             elif filt.filter_ == 'le':
-#                 temp_response = list(filter(lambda element: element[filt.column] <= filt.value, temp_response))
-#             elif filt.filter_ == 'e':
-#                 temp_response = list(filter(lambda element: element[filt.column] == filt.value, temp_response))
-#             elif filt.filter_ == 'eg':
-#                 temp_response = list(filter(lambda element: element[filt.column] >= filt.value, temp_response))
-#             elif filt.filter_ == 'g':
-#                 temp_response = list(filter(lambda element: element[filt.column] > filt.value, temp_response))
-#         if search:
-#             temp_response = list(filter(lambda element: search in element, temp_response))
-#         for i in range()
-#
-#     else:
-#         if not search:
-#             try:
-#                 for _ in range((page - 1) * count):
-#                     next(iterator)
-#                 for _ in range(count):
-#                     response.append(next(iterator))
-#             except StopIteration:
-#                 return response
-#         else:
-#             yielded = 0
-#             while yielded < page * count:
-#                 try:
-#                     row = next(iterator)
-#                     for col in row:
-#                         if search in col:
-#                             if yielded >= (page - 1) * count:
-#                                 response.append(row)
-#                             yielded += 1
-#                             break
-#                 except StopIteration:
-#                     break
-#     if sort > -1:
-#         response.sort(key=lambda column: column[sort], reverse=reverse)
-#     return response
-
 
 class Response(pydantic.BaseModel):
     rows: int
@@ -65,7 +16,10 @@ async def get_table_handler(page: int, count: int,
                             filters: FilterList = None) -> Response:
     all_rows = list(get_rows_from_csv())
     if sort > -1:
-        all_rows.sort(key=lambda element: element[sort], reverse=reverse)
+        try:
+            all_rows.sort(key=lambda element: int(element[sort]), reverse=reverse)
+        except ValueError:
+            all_rows.sort(key=lambda element: element[sort], reverse=reverse)
     if search:
         all_rows = list(filter(lambda element: search in element, all_rows))
     if filters:
